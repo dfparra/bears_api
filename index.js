2d506c08931ec59a7bc9f2892147b687705a06ac
Daniel <danielparra93@gmail.com>
2016-07-06 19:07:37 -0400

    POST PUT DELETE and GET routes working

--- index.js
+++ index.js
@@ -25,59 +25,94 @@ server.get('/bears', function(request, response){
   response.send(bears);
 });
 
-server.get('/bears/:id', function(request, response){
-  // response.send('GET bears :id');
-  //
-  // console.log(request.params);
+// server.get('/bears/:id', function(request, response){
+//   // response.send('GET bears :id');
+//   //
+//   // console.log(request.params);
+//
+//   var bear = db.get('bears')
+//                  .find({id: request.params.id})
+//                  .value();
+//   response.send(bear);
+// });
 
-  var bear = db.get('bears')
-                 .find({id: request.params.id})
-                 .value();
-  response.send(bear);
-});
+// server.get('/bears/:size', function(request, response){
+//   // response.send('GET bears :size');
+//
+//   var bear = db.get('bears')
+//                  .find({size: request.params.size})
+//                  .value();
+//   response.send(bear);
+//
+// });
 
-server.get('/bears/:size', function(request, response){
-  // response.send('GET bears :size');
-
-  var bear = db.get('bears')
-                 .find({size: request.params.size})
-                 .value();
-  response.send(bear);
-
-});
-
-server.get('/bears/:color', function(request, response){
-  response.send('GET bears :color');
-
-});
+// server.get('/bears/:color', function(request, response){
+//   // response.send('GET bears :color');
+//
+//   var bear = db.get('bears')
+//                  .find({color: request.params.color})
+//                  .value();
+//   response.send(bear);
+//
+// });
 
 server.get('/bears/:awake', function(request, response){
-  response.send('GET bears :awake');
+  // response.send('GET bears :awake');
+
+  var bear = db.get('bears')
+                 .find({isAwake: request.params.awake})
+                 .value();
+  response.send(bear);
 
 });
 
 server.get('/bears/:kids', function(request, response){
-  response.send('GET bears :kids');
+  // response.send('GET bears :kids');
+
+  var bear = db.get('bears')
+                 .find({hasKids: request.params.kids})
+                 .value();
+  response.send(bear);
 
 });
 
 server.get('/bears/:type', function(request, response){
-  response.send('GET bears :type');
+  // response.send('GET bears :type');
+
+  var bear = db.get('bears')
+                 .find({type: request.params.type})
+                 .value();
+  response.send(bear);
 
 });
 
 server.get('/bears/:hungry', function(request, response){
-  response.send('GET bears :hungry');
+  // response.send('GET bears :hungry');
+
+  var bear = db.get('bears')
+                 .find({isHungry: request.params.hungry})
+                 .value();
+  response.send(bear);
 
 });
 
 server.get('/bears/:notes', function(request, response){
-  response.send('GET bears :id');
+  // response.send('GET bears :id');
+
+  var bear = db.get('bears')
+                 .find({notes: request.params.notes})
+                 .value();
+  response.send(bear);
 
 });
 
 server.get('/bears/:gender', function(request, response){
-  response.send('GET bears :gender');
+  // response.send('GET bears :gender');
+
+  var bear = db.get('bears')
+                 .find({gender: request.params.gender})
+                 .value();
+  response.send(bear);
 
 });
 
@@ -89,10 +124,10 @@ server.post('/bears', function(request, response){
     id: uuid.v4(),
     size: request.body.size,
     color: request.body.color,
-    isAwake: false,
-    hasKids: false,
+    isAwake: false,//request.body.isAwake,
+    hasKids: false,//request.body.hasKids,
     type: request.body.type,
-    isHungry: false,
+    isHungry: false,//request.body.isHungry,
     notes: request.body.notes,
     gender: request.body.gender,
 
@@ -108,13 +143,37 @@ server.post('/bears', function(request, response){
 //UPDATE ROUTES
 
 server.put('/bears/:id', function(request, response){
-  response.send('PUT bears :id');
+  // response.send('PUT bears :id');
+
+  var updatedBearInfo = {
+    size: request.body.size,
+    color: request.body.color,
+    isAwake: request.body.isAwake,
+    hasKids: request.body.haskids,
+    type: request.body.type,
+    isHungry: request.body.isHungry,
+    notes: request.body.notes,
+    gender: request.body.gender
+  };
+
+  var updatedBear = db.get('bears')
+                      .find({id: request.params.id})
+                      .assign(updatedBearInfo)
+                      .value();
+  response.send(updatedBear);
+
 });
 
 //DELETE ROUTES
 
-server.delete('bears/:id', function(request, response){
-  response.send('DELETE bears :id');
+server.delete('/bears/:id', function(request, response){
+  // response.send('DELETE bears :id');
+
+  var bear = db.get('bears')
+                .remove({id: request.params.id})
+                .value();
+  response.send(bear);
+
 });
 
 server.listen(port, function(){
