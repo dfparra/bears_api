c06512df4f889de7f3e03bbb0d72a63e9958c12c
Daniel <danielparra93@gmail.com>
2016-07-05 20:31:20 -0400

    setup basic route methods

--- index.js
+++ index.js
@@ -16,12 +16,46 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/bears', function(request, response){
+  // response.send('GET bears!!!');
+
   var bears = db.get('bears')
                  .value();
   response.send(bears);
 });
 
+server.get('/bears/:id', function(request, response){
+  response.send('GET bears :id');
 
+  console.log(request.params);
+
+  // var bear = db.get('bears')
+  //                .find({id: request.params.id})
+  //                .value();
+  // response.send(bear);
+});
+
+server.post('/bears', function(request, response){
+  // response.send('POST bear');
+  var bear = {
+    id: uuid.v4(),
+    size: request.body.size,
+    isAwake: false
+  };
+
+  var result = db.get('bears')
+                 .push(bear)
+                 .last()
+                 .value();
+  response.send(result);
+});
+
+server.put('/bears/:id', function(request, response){
+  response.send('PUT bears :id');
+});
+
+server.delete('bears/:id', function(request, response){
+  response.send('DELETE bears :id');
+});
 
 server.listen(port, function(){
   console.log('Now listening on port: ',port);
